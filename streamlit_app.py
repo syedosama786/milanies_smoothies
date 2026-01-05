b35ffa3fb3171879ac2658ce75b71455a92e922c
syedosama786 <43749634+syedosama786@users.noreply.github.com>
2026-01-05 15:22:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
  
 # Write directly to the app
@@ -11,6 +11,7 @@ st.write(
 )
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be: ", name_on_order)
+ 
  
 cnx=st.connection("Snowflake") 
 session = cnx.session()
@@ -38,3 +39,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered! '+name_on_order, icon="✅")
+
+
+
+
+
